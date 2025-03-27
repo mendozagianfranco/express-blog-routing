363a89f6b62f2d74fa59988140984596f0317861
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-03-27 13:59:51 +0100

    feat: add assest static and import router posts

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const postsRouter = require('./routers/posts');
 
-
-
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Home Server');
 });
 
-
+app.use('/posts', postsRouter);
 
 app.listen(port, () => {
     console.log('App attiva su port: ' + port);
