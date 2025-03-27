dbbbe87b7ada584f85866cb61d624dbadb7b51ff
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-03-27 13:49:19 +0100

    feat: create router

---
+++ routers/posts.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+// INDEX
+router.get('/', (req, res) => {
+    res.send('Lista post');
+});
+
+// SHOW
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    res.send('Dettagli post ' + id);
+});
+
+// STORE
+router.post('/', (req, res) => {
+    res.send('Creazione post');
+});
+
+// UPDATE
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    res.send('Modifica integrale post ' + id);
+});
+
+// MODIFY
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    res.send('Modifica parziale post ' + id);
+});
+
+// DESTROY
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    res.send('Eliminazione post ' + id);
+});
+
+module.exports = router;
